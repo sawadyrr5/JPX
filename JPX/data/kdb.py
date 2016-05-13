2784d4ce3fdcc21f89376fc5d2ea22f3011f623f
RR4 <riskreturn5@gmail.com>
2016-05-14 02:24:14 +0900

    k-db.comの新サイト構成に対応

--- JPX/data/kdb.py
+++ JPX/data/kdb.py
@@ -1,264 +1,214 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
 import urllib.request
+import lxml.html
 from datetime import datetime
-
-__response = urllib.request.urlopen('http://old.k-db.com/?p=all&download=csv')
-__master = pd.read_csv(__response, encoding='Shift_JIS', skiprows=1)
-__master.set_index('コード', inplace=True)
+import pandas as pd
 
 
-def symbols():
+# 先物,指数,個別株,統計の基底クラス
+class __Historical:
+    _master_url = str()
+    _freq = tuple()
+    _category = str()
+    _session = tuple()
+    _xpath = dict()
+    _all_index_col = str()
+
+    _base_url = 'http://k-db.com/{category}'
+    _base_url_single = 'http://k-db.com/{category}/{symbol}/{freq}'
+    _base_url_single_dl = 'http://k-db.com/{category}/{symbol}/{freq}/{date}?download=csv'
+    _base_url_all = 'http://k-db.com/{category}/{date}{session}?download=csv'
+    _freq_format = {'1d': '%Y', '4h': '%Y', '1h': '%Y%m', '30m': '%Y%m', '15m': '%Y%m%d', '5m': '%Y%m%d'}
+
+    def __init__(self):
+        # カテゴリーの展開
+        param = dict(category=self._category)
+        self._base_url = self._base_url.format(**param)
+        root = _get_root(self._base_url)
+
+        # ドキュメントルートのparse用
+        def parse_root(xpath):
+            return [e.text for e in root.xpath(xpath)]
+
+        # コードのリスト作成
+        self.symbols = [symbol.split('/')[-1] for symbol in root.xpath(self._xpath['symbols'])]
+
+        # コード名称の辞書作成
+        self.names = dict(zip(self.symbols, parse_root(self._xpath['names'])))
+
+        # 先物限月の辞書作成, エラーの場合無視
+        try:
+            self.contracts = dict(zip(self.symbols, parse_root(self._xpath['contracts'])))
+        except KeyError:
+            pass
+
+    def price(self, date_from: datetime, date_to: datetime, symbol: str, freq: str) -> pd.DataFrame:
+        """
+        Download historical price of specified symbol.
+        :param date_from:
+        :param date_to:
+        :param symbol:
+            Specify symbol.
+        :param freq:
+            Specify frequency.
+        :return: pandas.DataFrame
+        """
+        # symbolが存在しなかった場合は中断
+        if symbol not in self.symbols:
+            raise KDBError("specified symbol is not found in this category.")
+
+        # freqがリストに無い場合は中断
+        if freq not in self._freq:
+            raise KDBError("specified freq is not available.")
+
+        # 取得可能なdate_rangeを抽出
+        params = dict(category=self._category, symbol=symbol, freq=freq)
+        url = self._base_url_single.format(**params)
+        root = _get_root(url)
+        date_range_web = set([date.split('/')[-1] for date in root.xpath(self._xpath['date_range'])])
+        date_range_f = _formatted_date_range(date_from, date_to, self._freq_format[freq])
+        date_range = date_range_web.intersection(date_range_f)
+
+        # 期間がとれない場合は処理中断
+        if not date_range:
+            return pd.DataFrame()
+
+        params = [dict(category=self._category, date=date, symbol=symbol, freq=freq) for date in date_range]
+        urls = [self._base_url_single_dl.format(**param) for param in params]
+        dfs = _fetch_csv(urls)
+
+        df = pd.concat(dfs)
+        df['日付'] = pd.to_datetime(df['日付'])
+
+        if freq in ('1d', '4h'):
+            index_level = ['日付']
+            df = df.set_index(keys=index_level)
+            df = df.ix[min(df[df.index >= date_from].index):max(df[df.index <= date_to].index)]
+        else:
+            index_level = ['日付', '時刻']
+            df = df.set_index(keys=index_level)
+
+        df = df.sort_index(level=index_level, ascending=True)
+        return df
+
+    def price_all(self, date_from: datetime, date_to: datetime, session=None) -> pd.DataFrame:
+        """
+        Download historical price of all symbols.
+        :param date_from:
+        :param date_to:
+        :param session:
+            Specify session.
+        :return: pandas.DataFrame
+        """
+        # sessionが指定されなかった場合または適合しない場合は''とする
+        if session in self._session:
+            session = '/' + str(session)
+        else:
+            session = ''
+
+        date_range = _formatted_date_range(date_from, date_to, '%Y-%m-%d')
+        params = [dict(category=self._category, date=date, session=session) for date in date_range]
+
+        urls = [self._base_url_all.format(**param) for param in params]
+        dfs = _fetch_csv(urls)
+
+        # 取得結果がすべて空だった場合は空DataFrameを返して終了
+        if all([df.empty for df in dfs]):
+            return pd.DataFrame()
+
+        # DataFrameに日付列を追加する
+        def add_date(df: pd.DataFrame, date: datetime):
+            df['日付'] = date
+            return df
+
+        dfs = [add_date(df, date) for df, date in zip(dfs, date_range) if not df.empty]
+
+        df = pd.concat(dfs)
+        df = df.set_index(keys=['日付', self._all_index_col]).sort_index()
+        return df
+
+
+def _get_root(url):
     """
-    Return all symbols.
+    ドキュメントルートを取得する(parse用)
     """
-    result = list(__master.index)
-    return result
+    response = urllib.request.urlopen(url).read().decode('utf-8-sig')
+    return lxml.html.fromstring(response)
 
 
-def historical(symbol, interval=None, start=None, end=None):
+def _formatted_date_range(date_from, date_to, date_format):
     """
-    Download historical data of stock, future, index and statistics from k-db.com.
-
-    :param symbol: stock, index, future or statistics symbol
-    :param interval: d, a, m5, m
-    :param start: when interval is 'd' or 'a', start date.
-                  when interval is 'm5' or 'm', specify date.
-    :param end: when interval is 'd' or 'a', end date.
-                when interval is 'm5' or 'm', ignored.
-    :return: pandas.DataFrame
+    ドキュメントルートを取得する(parse用)
     """
-    if isinstance(start, datetime):
-        if isinstance(end, datetime) and interval in ('d', 'a') and start > end:
-            end = start
-    else:
-        return
-
-    # make url list
-    urls = __make_urls(symbol, interval, start, end)
-
-    # fetch by url list
-    df = pd.concat([__fetch_csv(url) for url in urls])
-
-    # set index
-    df = __set_index(df, symbol, interval)
-
-    # truncate outer period
-    if interval in ('d', 'a'):
-        result = df.ix[start:end]
-    else:
-        result = df
-    return result
+    return set([datetime.strftime(date, date_format) for date in pd.date_range(date_from, date_to, freq='D')])
 
 
-class __SymbolType:
-    # define columns sequence.
-    column_type = dict(
-        all=('Symbol', 'Name', 'Exchange', 'Sector', 'Open', 'High', 'Low', 'Close', 'Volume', 'DollarVolume'),
-        day=('Date', 'Open', 'High', 'Low', 'Close'),
-        day_v=('Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'DollarVolume'),
-        session=('Date', 'Session', 'Open', 'High', 'Low', 'Close'),
-        session_v=('Date', 'Session', 'Open', 'High', 'Low', 'Close', 'Volume', 'DollarVolume'),
-        minute=('Date', 'Time', 'Open', 'High', 'Low', 'Close'),
-        minute_v=('Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'DollarVolume'),
-        statistics=('Date', 'Volume', 'DollarVolume', 'Numbers', 'Pricing', 'Up', 'Unchange', 'Down', 'Incomparable')
+def _fetch_csv(urls):
+    """
+    CSVを取得する
+    """
+    if not urls:
+        return []
+
+    def read_csv(url):
+        try:
+            df = pd.read_csv(urllib.request.urlopen(url), encoding='Shift_JIS')
+        except ValueError or urllib.error.HTTPError:
+            df = pd.DataFrame()
+        return df
+
+    dfs = [read_csv(url) for url in urls]
+    return dfs
+
+
+class Futures(__Historical):
+    _category = 'futures'
+    _freq = ('1d', '4h', '1h', '30m', '15m', '5m')
+    _session = ('e',)
+    _xpath = dict(
+        symbols='//*[@id="maintable"]/tbody//tr/td[4]/a/@href',
+        names='//*[@id="maintable"]/tbody//tr/td[1]',
+        contracts='//*[@id="maintable"]/tbody//tr/td[4]/a',
+        date_range='//*[@id="contentmain"]/div[3]//div/a/@href'
     )
+    _all_index_col = '先物'
 
-    # define index sequence.
-    index_type = dict(
-        day=['Date'],
-        session=['Date', 'Session'],
-        minute=['Date', 'Time']
+
+class Indices(__Historical):
+    _category = 'indices'
+    _freq = ('1d', '4h', '1h', '30m', '15m', '5m')
+    _session = ('a', 'b')
+    _xpath = dict(
+        symbols='//*[@id="maintable"]/tbody//tr/td[1]/a/@href',
+        names='//*[@id="maintable"]/tbody//tr/td[1]/a',
+        date_range='//*[@id="contentmain"]/div[3]//div/a/@href'
     )
-
-    @property
-    def index(self):
-        return
-
-    @property
-    def column(self):
-        return
+    _all_index_col = '指数'
 
 
-class __Stocks(__SymbolType):
-    category = 'stocks'
-
-    def __init__(self, interval):
-        self.interval = interval
-        return
-
-    @property
-    def index(self):
-        result = dict(
-            d=self.index_type['day'],
-            a=self.index_type['session'],
-            m5=self.index_type['minute'],
-            m=self.index_type['minute']
-        )
-        return result[self.interval]
-
-    @property
-    def column(self):
-        result = dict(
-            d=self.column_type['day_v'],
-            a=self.column_type['session_v'],
-            m5=self.column_type['minute_v'],
-            m=self.column_type['minute_v']
-        )
-        return result[self.interval]
-
-
-class __Indices(__SymbolType):
-    category = 'indices'
-
-    def __init__(self, interval):
-        self.interval = interval
-        return
-
-    @property
-    def index(self):
-        result = dict(
-            d=self.index_type['day'],
-            a=self.index_type['session'],
-            m5=self.index_type['minute']
-        )
-        return result[self.interval]
-
-    @property
-    def column(self):
-        result = dict(
-            d=self.column_type['day'],
-            a=self.column_type['session'],
-            m5=self.column_type['minute']
-        )
-        return result[self.interval]
-
-
-class __Futures(__SymbolType):
-    category = 'futures'
-
-    def __init__(self, interval):
-        self.interval = interval
-        return
-
-    @property
-    def index(self):
-        result = dict(
-            d=self.index_type['session'],
-            a=self.index_type['session'],
-            m5=self.index_type['minute'],
-            m=self.index_type['minute']
-        )
-        return result[self.interval]
-
-    @property
-    def column(self):
-        result = dict(
-            d=self.column_type['session_v'],
-            a=self.column_type['session_v'],
-            m5=self.column_type['minute_v'],
-            m=self.column_type['minute_v']
-        )
-        return result[self.interval]
-
-
-class __Statistics(__SymbolType):
-    category = 'statistics'
-
-    def __init__(self, interval):
-        self.interval = interval
-        return
-
-    @property
-    def index(self):
-        result = dict(
-            d=self.index_type['day']
-        )
-        return result[self.interval]
-
-    @property
-    def column(self):
-        result = dict(
-            d=self.column_type['statistics'],
-        )
-        return result[self.interval]
-
-
-def __get_symbol_type(symbol, interval):
-    # define statistic symbols
-    symbols_stat = (
-        'T1', 'T2', 'TM', 'JQS',
-        'T1-I201', 'T1-I202', 'T1-I203', 'T1-I204', 'T1-I205', 'T1-I206', 'T1-I207', 'T1-I208', 'T1-I209', 'T1-I210',
-        'T1-I211', 'T1-I212', 'T1-I213', 'T1-I214', 'T1-I215', 'T1-I216', 'T1-I217', 'T1-I218', 'T1-I219', 'T1-I220',
-        'T1-I221', 'T1-I222', 'T1-I223', 'T1-I224', 'T1-I225', 'T1-I226', 'T1-I227', 'T1-I228', 'T1-I229', 'T1-I230',
-        'T1-I231', 'T1-I232', 'T1-I233'
+class Stocks(__Historical):
+    _category = 'stocks'
+    _freq = ('1d', '4h', '1h', '30m', '15m', '5m')
+    _session = ('a', 'b')
+    _xpath = dict(
+        symbols='//*[@id="maintable"]/tbody//tr/td[1]/a/@href',
+        names='//*[@id="maintable"]/tbody//tr/td[1]/a',
+        date_range='//*[@id="contentmain"]/div[3]//div/a/@href'
     )
-
-    if symbol in symbols_stat:
-        result = __Statistics(interval)
-    elif any(__master[__master['業種'] == '先物'].index.isin([symbol])):
-        result = __Futures(interval)
-    elif any(__master[__master['業種'] == '指数'].index.isin([symbol])):
-        result = __Indices(interval)
-    else:
-        result = __Stocks(interval)
-
-    return result
+    _all_index_col = 'コード'
 
 
-# set index
-def __set_index(df, symbol, interval):
-    symbol_type = __get_symbol_type(symbol, interval)  # select historical data type
-
-    df.columns = symbol_type.column  # set column name
-    if interval in ('d', 'a'):
-        df.set_index(pd.to_datetime(df['Date']), inplace=True)
-        df.drop('Date', axis=1, inplace=True)
-    elif interval in ('m5', 'm'):
-        df['Date'] = df['Date'] + ' ' + df['Time']
-        df.set_index(pd.to_datetime(df['Date']), inplace=True)
-        df.drop(symbol_type.index, axis=1, inplace=True)
-
-    result = df.sort_index(ascending=True)
-    return result
-
-
-# making url string.
-def __make_urls(symbol, interval, start, end):
-    symbol_type = __get_symbol_type(symbol, interval)
-
-    url_base = 'http://old.k-db.com/{category}/{symbol}{interval}?{date}download=csv'
-    url_interval = dict(
-        d='',
-        a='/a',
-        m5='/5min',
-        m='/minutely'
+class Statistics(__Historical):
+    _category = 'statistics'
+    _freq = ('1d',)
+    _session = []
+    _xpath = dict(
+        symbols='//*[@id="maintable"]/tbody//tr/td[1]/a/@href',
+        names='//*[@id="maintable"]/tbody//tr/td[1]/a',
+        date_range='//*[@id="contentmain"]/div[2]//div/a/@href'
     )
-    params = [dict(
-        category=symbol_type.category,
-        symbol=symbol,
-        interval=url_interval[interval],
-        date=date
-    ) for date in __make_date_string(interval, start, end)]
-
-    result = [url_base.format(**param) for param in params]
-    return result
+    _all_index_col = '市場'
 
 
-# making date part string.
-def __make_date_string(interval, start, end):
-    if interval in ('d', 'a') and isinstance(start, datetime) and isinstance(end, datetime):
-        result = ['year=' + str(year) + '&' for year in range(start.year, end.year + 1)]
-    elif interval in ('m5', 'm') and isinstance(start, datetime):
-        result = ['date=' + datetime.strftime(start, '%Y-%m-%d') + '&']
-    else:
-        result = []
-
-    return result
-
-
-# fetch csv data from k-db.com
-def __fetch_csv(url):
-    response = urllib.request.urlopen(url)
-    result = pd.read_csv(response, encoding='Shift_JIS', skiprows=1)
-    return result
+class KDBError(Exception):
+    pass
